e9576ceeef81f3820615fb0c861c8f3f224490b6
Yash Katariya <yashkatariya@google.com>
2023-03-15 20:06:42 -0700

    Remove references to `jax.config.jax_jit_pjit_api_merge`, which is always True at head.
    PiperOrigin-RevId: 516998437

--- oryx/core/interpreters/harvest_test.py
+++ oryx/core/interpreters/harvest_test.py
@@ -397,10 +397,7 @@ class PlantTest(test_util.TestCase):
 
     jaxpr = jax.make_jaxpr(plant_variables(f))({'x': 2.}, 1.)
     primitives = set(eqn.primitive for eqn in jaxpr.jaxpr.eqns)
-    if jax.config.jax_jit_pjit_api_merge:
-      self.assertIn(pjit.pjit_p, primitives)
-    else:
-      self.assertIn(jax.xla.xla_call_p, primitives)
+    self.assertIn(pjit.pjit_p, primitives)
 
   def test_should_plant_in_pmap(self):
 
