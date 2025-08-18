4103b6f2ab86315f0fdf9dd1d561126dd50a51fd
Jake VanderPlas <vanderplas@google.com>
2025-08-18 11:42:50 -0700

    [oryx] Switch to internal ad APIs
    A number of APIs in `jax.interpreters.ad` are deprecated as of JAX version 0.7.1.
    
    PiperOrigin-RevId: 796512018

--- oryx/core/interpreters/harvest.py
+++ oryx/core/interpreters/harvest.py
@@ -155,10 +155,10 @@ from jax._src import core as jax_core
 from jax._src import effects
 from jax._src import sharding_impls
 from jax._src import util as jax_util
+from jax._src.interpreters import ad
 from jax._src.lax import control_flow as lcf
 import jax.extend as jex
 import jax.extend.linear_util as lu
-from jax.interpreters import ad
 from jax.interpreters import batching
 from jax.interpreters import mlir
 from jax.interpreters import partial_eval as pe
