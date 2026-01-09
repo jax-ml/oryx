4216cde252e5deb7789e851d2bff4a680be36623
Yash Katariya <yashkatariya@google.com>
2026-01-08 20:48:25 -0800

    Fix FlatTree usage in oryx
    PiperOrigin-RevId: 853999012

--- oryx/core/interpreters/harvest.py
+++ oryx/core/interpreters/harvest.py
@@ -831,7 +831,7 @@ def _initial_style_jaxpr(
     in_avals: list[jax_core.AbstractValue],
     debug_info: jax_core.DebugInfo,
 ) -> tuple[jax_core.ClosedJaxpr, list[Any], tree_util.PyTreeDef]:
-  in_flat_tree = FlatTree.pack((FlatTree(in_avals, in_tree), {}))
+  in_flat_tree = FlatTree.pack((FlatTree(in_avals, in_tree, False), {}))
   jaxpr, out_flat_tree = pe.trace_to_jaxpr(fun, in_flat_tree, debug_info)
   jaxpr_no_constvars, consts = pe.separate_consts(jaxpr)
   return jaxpr_no_constvars, consts, out_flat_tree.tree
@@ -842,7 +842,7 @@ def _initial_style_jaxprs_with_common_consts(
     in_tree: tree_util.PyTreeDef,
     in_avals: list[jax_core.AbstractValue | jax_core.AvalQDD],
     debug_infos: list[jax_core.DebugInfo]):
-  in_flat_tree = FlatTree.pack((FlatTree(in_avals, in_tree), {}))
+  in_flat_tree = FlatTree.pack((FlatTree(in_avals, in_tree, False), {}))
   jaxprs_, out_flat_trees = zip(*[pe.trace_to_jaxpr(
       f, in_flat_tree, dbg) for f, dbg in zip(funs, debug_infos)])
   jaxprs_, all_consts = zip(*[pe.separate_consts(j) for j in jaxprs_])
